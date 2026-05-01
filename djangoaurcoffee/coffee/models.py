444281961d9a7a8f76ca8d6e76dbfe60c0220b71
rek75625 <rekhan7562@gmail.com>
2026-05-01 15:41:38 +0500

    media handlings

--- djangoaurcoffee/coffee/models.py
+++ djangoaurcoffee/coffee/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class CoffeeVary(models.Model):
+    COFFEE_TYPE_CHOICESN =[("ES","ESPRESSO"),
+                           ("LT","LATTE"),
+                           ("MH","MACCHIATO"),
+                           ("FW","FLAT WHITEZ"),
+                           ("CP","CAPPUCCINO"),]
+
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='coffees/')
+    date_time = models.DateTimeField(default=timezone.now)
+    types = models.CharField(max_length=2,choices=COFFEE_TYPE_CHOICESN)
